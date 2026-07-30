193ec692be70213e697b6744b3f302c8fddfe7b0
BennyWangsihao <162288670+BennyWangsihao@users.noreply.github.com>
2026-07-30 02:17:14 +0000

    1

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // document.body.style.backgroundcolour="red"
 //find my test button
+const testbutton = document.getElementById("test-button")
+
+//do somthing when we click the button
+testbutton. addEventListener("click", playTestNote);
+
+function playTestNote({show usage new Promise((resolve, reject) => {
+    
+})})
